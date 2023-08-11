74a2192a4770e03b4bea05a4c3b77b881ea77f47
vohidiprofticket <vohidiprofticket@gmail.com>
2023-08-11 16:33:58 +0300

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from './components/Header.vue';
+import Main from './components/Main.vue';
 </script>
 
 <template>
   <div class="home">
     <Header> </Header>
+    <Main></Main>
   </div>
 </template>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,42 @@
 <template>
-    <div>
-        
+    <div class="main">
+        <div class="container">
+            <div class="tab">
+                <div class="content">
+                    <div class="left-content">
+                    </div>
+                    <div class="right-content">
+                        <div class="tab-header">
+                            asdasdsdaffffffffffffffffffffffffffffffffffffffffff
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
 
 </script>
 <style lang="scss">
-    
+.contanier {
+    max-width: 1312px;
+    width: 100%;
+    margin: auto;
+}
+
+.content {
+    display: flex;
+    align-items: start;
+    justify-content: space-between;
+    box-sizing: border-box;
+
+    .right-content {
+        width: 75%;
+    }
+
+    .left-content {
+        width: 25%;
+    }
+}
 </style>
