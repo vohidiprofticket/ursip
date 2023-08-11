e294657d9782e672295101445dda5520e0187601
vohidiprofticket <vohidiprofticket@gmail.com>
2023-08-11 15:36:37 +0300

    new project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div class="home">
+    <Header> </Header>
+  </div>
+</template>
+
+<style lang="scss"></style>
